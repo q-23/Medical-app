29b6bda2f4df5176cc0fcda18cfee8ea781b97e2
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2020-05-19 11:44:32 +0200

    Inserting images into container

--- js/photo.js
+++ js/photo.js
@@ -1,3 +1,5 @@
-export default function () {
-
+export default function (imageUrl) {
+    return `
+        <img src="${imageUrl}"/>
+    `;
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 import credentials from '../credentials.js';
 import photo from './photo.js';
-console.log(credentials)
 const container = document.querySelector('div.container');
 
 const { client_id, client_secret, grant_type, refresh_token } = credentials;
 
-    (async () => {
-        const googleApiResponse = await fetch(`https://oauth2.googleapis.com/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=${grant_type}&refresh_token=${refresh_token}`, {
-            method: 'POST'
-        });
+(async () => {
+    const googleApiResponse = await fetch(`https://oauth2.googleapis.com/token?client_id=${client_id}&client_secret=${client_secret}&grant_type=${grant_type}&refresh_token=${refresh_token}`, {
+        method: 'POST'
+    });
 
-        console.log(await googleApiResponse.json()) 
-    })()
+    const { access_token } = await googleApiResponse.json();
+
+    const googlePhotosResponse = await fetch('https://photoslibrary.googleapis.com/v1/mediaItems', {
+        headers: {
+            'Authorization': `Bearer ${access_token}`,
+        }
+    });
+
+    const { mediaItems } = await googlePhotosResponse.json();
+    const photosHTMLinput = mediaItems.map(({baseUrl}) => photo(baseUrl)).join('')
+    container.innerHTML = photosHTMLinput;
+
+})()
