137184899466503cf2076f05b4ff37b4b0aeecf0
Aleksander Kwiatkowski <aleksander.kwiatkowski@gmail.com>
2020-05-19 13:37:40 +0200

    Update gitignore

---
+++ credentials.js
@@ -0,0 +1,6 @@
+export default {
+    client_id: '340814031012-kansdkv6c10d9qktccihra8lneo8ub4s.apps.googleusercontent.com',
+    client_secret: 'wMdf96SrqVhu8SscDilMUUiM',
+    grant_type: 'refresh_token',
+    refresh_token: '1//0cvkPsabjsEX4CgYIARAAGAwSNwF-L9IrCh-eMWwcqGHkfVEMiHDxSq8yp57AqSWJ9h162P0p8rl0JNimpqbsqhqICTz-ylb_tVQ'
+}
